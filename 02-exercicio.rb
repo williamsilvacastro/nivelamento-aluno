7e361b53b411f4d4f8ad8fb17b0ebf97b8b4020f
williamsilvacastro <williams.castro2001@gmail.com>
2021-07-08 16:56:54 -0300

    variaveis

---
+++ 02-exercicio.rb
@@ -0,0 +1,12 @@
+horasPorAno=365*24
+minutosEmDecada=10*365*24*60
+segundosDevida=20*365*24*60*60
+chocolatesVitalicios=150*80
+idadeMilhonaria=(((1232000000.0/60)/60)/24)/365
+puts "um ano tem "+ horasPorAno.to_s+" horas"
+puts "uma decada tem "+minutosEmDecada.to_s+" minutos"
+puts "eu tenho "+ segundosDevida.to_s+" segundos de vida"
+puts "eu pretendo comer "+chocolatesVitalicios.to_s+" chocolates durante a minha vida xd"
+puts "se a sua idadde é 1232 milhoes de segundos, voce tem "+idadeMilhonaria.to_s+" anos de vida"
+
+
