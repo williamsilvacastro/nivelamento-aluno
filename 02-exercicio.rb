544a127ac342d95de2df300aff8b14320df94c75
williamsilvacastro <williams.castro2001@gmail.com>
2021-07-08 18:05:51 -0300

    variaveis melhorado

--- 02-exercicio.rb
+++ 02-exercicio.rb
@@ -3,11 +3,12 @@ horasNoDia=24
 minutosEmUmaHora=60
 segundosEmUmMinuto=60
 expectativaEmAnosDeVida=80
+idadeEmSegundos= 1232000000.0
 horasPorAno=diasNoAno*horasNoDia
 minutosEmDecada=10*diasNoAno*horasNoDia*minutosEmUmaHora
 segundosDevida=20*diasNoAno*horasNoDia*minutosEmUmaHora*segundosEmUmMinuto
 chocolatesVitalicios=150*expectativaEmAnosDeVida
-idadeMilhonaria=(((1232000000.0/segundosEmUmMinuto)/minutosEmUmaHora)/horasNoDia)/diasNoAno
+idadeMilhonaria=(((idadeEmSegundos/segundosEmUmMinuto)/minutosEmUmaHora)/horasNoDia)/diasNoAno
 puts "um ano tem "+ horasPorAno.to_s+" horas"
 puts "uma decada tem "+minutosEmDecada.to_s+" minutos"
 puts "eu tenho "+ segundosDevida.to_s+" segundos de vida"
