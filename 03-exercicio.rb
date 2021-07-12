20edfc81d887836fa3f770ec0b689749ca6c2b07
williamsilvacastro <williams.castro2001@gmail.com>
2021-07-12 11:54:27 -0300

    12/07 commit

--- 03-exercicio.rb
+++ 03-exercicio.rb
@@ -15,6 +15,6 @@ def converter_segundos_em_anos(segundos, segundosEmUmAno)
     return segundos/segundosEmUmAno
 end
 puts "um ano tem "+ converter_anos_em_horas(1, horasEmUmAno).to_s+" horas"
-puts "uma decada tem "+converter_decadas_em_segundos(1, minutosEmUmaDecada).to_s+" minutos"
+puts "uma decada tem "+converter_decadas_em_minutos(1, minutosEmUmaDecada).to_s+" minutos"
 puts "eu tenho "+ converter_anos_em_segundos(20, segundosEmUmAno).to_s+" segundos de vida"
 puts "se a sua idade é 1232 milhoes de segundos, voce tem "+converter_segundos_em_anos(1232000000, segundosEmUmAno).to_s+" anos de vida"
