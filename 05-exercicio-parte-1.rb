684852ab5b0aae0b3da5fd98b4c19c27870baca8
williamsilvacastro <williams.castro2001@gmail.com>
2021-07-13 14:48:20 -0300

    13/07 commit

--- 05-exercicio-parte-1.rb
+++ 05-exercicio-parte-1.rb
@@ -13,3 +13,12 @@ puts "se o raio de um circulo é :"+raio.to_s
 puts "seu diametro é :"+calc_diametro(raio).to_s
 puts "seu comprimento é :"+calc_comprimento(raio).to_s
 puts "sua area é :"+calc_area(raio).to_s
+
+# Exercicio 05
+# Parte 1:
+# Considere os métodos (as funções) escritos nos exercícios anteriores:
+# Os métodos que calculam:
+# - o diâmetro de um círculo é 2x o seu raio
+# - o comprimento de um círculo é seu diâmetro vezes o valor da constante matemática PI (3.1415...)
+# - a área de um círculo é seu raio ao quadrado vezes o valor da constante matemática PI (3.1415...)
+# Escreva um programa que use esses métodos, porém, de tal forma que o valor do raio do círculo seja perguntado ao usuário.
--- 05-exercicio-parte-2.rb
+++ 05-exercicio-parte-2.rb
@@ -10,3 +10,7 @@ def calc_batimentos(idade, minutos_em_um_ano)
     return converter_anos_em_minutos(idade, minutos_em_um_ano)*80
 end 
 print "o coração do "+nome+" bateu aproximadamente "+calc_batimentos(idade, minutos_em_um_ano).to_s+" vezes durante a sua vida"
+
+# Parte 2:
+# Escreva um programa que pergunte o nome e a idade do usuário. Escreva também uma função que calcula a quantidade de batimentos cardíacos (uma estimativa) baseado na quantidade de dias que o usuário viveu. Considere uma média de 80 batimentos por minuto. Essa função deve receber a idade do usuário como parâmetro.
+# Para cada parte, escreva códigos que testem os programas escritos.
