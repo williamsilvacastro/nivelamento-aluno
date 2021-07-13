a6bbd569d0f6c4e0278b8ab9020b2416b08bdeec
williamsilvacastro <williams.castro2001@gmail.com>
2021-07-13 12:12:37 -0300

    13/07 commit

--- 05-exercicio-parte-2.rb
+++ 05-exercicio-parte-2.rb
@@ -9,4 +9,4 @@ end
 def calc_batimentos(idade, minutos_em_um_ano)
     return converter_anos_em_minutos(idade, minutos_em_um_ano)*80
 end 
-print " o coração do "+nome+" bateu aproximadamente "+calc_batimentos(idade, minutos_em_um_ano).to_s+" vezes durante a sua vida"
+print "o coração do "+nome+" bateu aproximadamente "+calc_batimentos(idade, minutos_em_um_ano).to_s+" vezes durante a sua vida"
