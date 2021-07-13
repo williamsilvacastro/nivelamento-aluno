178eaec8ce673e9a6fa3f7f11b837af1802b1568
williamsilvacastro <williams.castro2001@gmail.com>
2021-07-13 12:11:37 -0300

    13/07 commit

--- 05-exercicio-parte-2.rb
+++ 05-exercicio-parte-2.rb
@@ -1,6 +1,6 @@
 minutos_em_um_ano=525600
 print "qual o seu nome:"
-nome = gets
+nome = gets.chomp
 print "qual a sua idade:"
 idade= gets.to_f
 def converter_anos_em_minutos(anos, minutos_em_um_ano)
@@ -9,4 +9,4 @@ end
 def calc_batimentos(idade, minutos_em_um_ano)
     return converter_anos_em_minutos(idade, minutos_em_um_ano)*80
 end 
-puts nome+" o seu coração bateu aproximadamente "+calc_batimentos(idade, minutos_em_um_ano).to_s+" vezes durante a sua vida"
+print " o coração do "+nome+" bateu aproximadamente "+calc_batimentos(idade, minutos_em_um_ano).to_s+" vezes durante a sua vida"
