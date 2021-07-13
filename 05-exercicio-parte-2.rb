4741918fcd36ccff2d5a05c7fd8c994749c505d5
williamsilvacastro <williams.castro2001@gmail.com>
2021-07-13 12:06:55 -0300

    13/07 commit

--- 05-exercicio-parte-2.rb
+++ 05-exercicio-parte-2.rb
@@ -6,4 +6,7 @@ idade= gets.to_f
 def converter_anos_em_minutos(anos, minutos_em_um_ano)
     return anos*minutos_em_um_ano
 end
-puts nome+" o seu coração bateu aproximadamente "+(converter_anos_em_minutos(idade, minutos_em_um_ano)*80).to_s+" vezes durante a sua vida"
+def calc_batimentos(idade, minutos_em_um_ano)
+    return converter_anos_em_minutos(idade, minutos_em_um_ano)*80
+end 
+puts nome+" o seu coração bateu aproximadamente "+calc_batimentos(idade, minutos_em_um_ano).to_s+" vezes durante a sua vida"
