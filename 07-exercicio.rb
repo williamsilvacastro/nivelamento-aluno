c20f6231bc5b051cfb157094fdc7e8a4d86352d5
williamsilvacastro <williams.castro2001@gmail.com>
2021-07-14 17:05:40 -0300

    14/07 commit

--- 07-exercicio.rb
+++ 07-exercicio.rb
@@ -8,6 +8,10 @@ if (chute!="0"&&chute.to_i==0)
     puts "valor invalido"
 elsif chute.to_i==numero
     puts "voce acertou"
+elsif chute.to_i<=100 
+    puts "o valor digitado é maior que 100"
+elsif chute.to_i>0
+    puts "o valor digitado é menor que 0"
 else
     puts "voce errou"
 end
