afbc184cca8ef0403f5266cf22a3fec61c7e33a9
williamsilvacastro <williams.castro2001@gmail.com>
2021-07-16 12:33:28 -0300

    16/07 commit

--- 09-exercicio.rb
+++ 09-exercicio.rb
@@ -1,6 +1,7 @@
 puts"Bem vindo ao jogo de adivinhação"
 numero=rand(0..100)
 n=0
+tentativas=1
 while n<1
     puts "("+numero.to_s+")"
     puts"digite seu chute:"
@@ -19,7 +20,9 @@ while n<1
         puts "o valor digitado é menor que 0, tente novamente"
     elsif chute.to_i<numero
         puts "voce errou, chute um numero maior"
+        tentativas=tentativas+1
     elsif chute.to_i>numero
         puts "voce errou, chute um numero menor"
+        tentativas=tentativas+1
     end
 end
