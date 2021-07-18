921343018869178fff3babcafa8d1d450a10ac2d
williamsilvacastro <williams.castro2001@gmail.com>
2021-07-18 15:54:42 -0300

    commit 18/07

--- 10-exercicio-parte-4.rb
+++ 10-exercicio-parte-4.rb
@@ -1,12 +1,30 @@
-
-
-
-
-
-
-
-
-
+for x in(1..9)
+    for y in(1..17)
+        if(x==y || x+y==18)
+            print "_"
+        else
+            print"*"
+        end
+    end 
+    puts
+end
+# 1,1
+# 2,2
+# 3,3
+# 4,4
+# 5,5
+# 6,6
+# 7,7
+# 8,8
+# 9,9
+# 10,8
+# 11,7
+# 12,6
+# 13,5
+# 14,4
+# 15,3
+# 16,2
+# 17,1
 
 
 # 4) Quer mais? Tente fazer uma letra V, usando asteriscos “*” e
